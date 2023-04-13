17d19f7ffe6f24b002f4e92c968d8f6152e70af6
itshmnt <hemantxs5@gmail.com>
2023-04-13 17:24:23 +0530

    Adding Question component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import Questions from './Questions';
 
 export default function Quiz() {
+
+    function onNext(){
+        console.log('Next');
+    }
+
+    function onPrev(){
+        console.log('Prev');
+    }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quizzy : Quiz App</h1>
+
+        <Questions />
+
+        <div className='grid'>
+            <button className='btn prev' onClick={onPrev}>Prev</button>
+            <button className='btn next' onClick={onNext}>Next</button>
+        </div>
+
+    </div>
   )
 }
