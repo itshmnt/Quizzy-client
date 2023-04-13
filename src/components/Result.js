9cf83e34981847e4c2bc7c32cdd5790794caf5bb
itshmnt <hemantxs5@gmail.com>
2023-04-13 17:45:25 +0530

    Added Result & Result table

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,50 @@
 import React from 'react'
-
+import '../styles/Result.css'
+import { Link } from 'react-router-dom'
+import ResultTable from './ResultTable';
 export default function Result() {
+
+    function onRestart () {
+        console.log('Restart button clicked');
+    }
   return (
-    <div>Result</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quizzy : Quiz App</h1>
+
+        <div className='result flex-center'>
+            <div className='flex'>
+                <span>Username</span>
+                <span className='bold'> Daily Quiz</span>
+            </div>
+            <div className='flex'>
+                <span>Total Quiz Points : </span>
+                <span className='bold'>50</span>
+            </div>
+            <div className='flex'>
+                <span>Total Questions : </span>
+                <span className='bold'>5</span>
+            </div>
+            <div className='flex'>
+                <span>Total Attempts : </span>
+                <span className='bold'>03</span>
+            </div>
+            <div className='flex'>
+                <span>Total Earned Points : </span>
+                <span className='bold'>50</span>
+            </div>
+            <div className='flex'>
+                <span>Quiz Result : </span>
+                <span className='bold'>Passed</span>
+            </div>
+        </div>
+
+        <div className='start'>
+            <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+        </div>
+
+        <div className='container'>
+            <ResultTable />
+        </div>
+    </div>
   )
 }
